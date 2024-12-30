69df66a2e7e16c9f01ef3dcd0d397aa70e5b093f
weixiaoqiang <1839992361@qq.com>
2024-12-30 10:03:39 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory,createWebHashHistory } from 'vue-router'
 import Layout from '../components/layout/index.vue';
 import AdminLayout from '../components/layout/admin/index.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
