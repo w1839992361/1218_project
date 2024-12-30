4235868d86c4b0bce93be2bb9f788908e912b08a
weixiaoqiang <1839992361@qq.com>
2024-12-30 14:46:59 +0800

    update

--- src/api/admin/content.js
+++ src/api/admin/content.js
@@ -81,13 +81,6 @@ export function uploadDocs(params, data) {
   })
 }
 
-// export function getVideoUUID(id) {
-//   return request({
-//     url: `/api/videos/getVideoUUID/${id}`,
-//     method: 'get',
-//   })
-// }
-
 export function getDocsInfo(id) {
   return request({
     url: `/api/docs/info/${id}`,
