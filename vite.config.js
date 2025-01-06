79fc8a205e14e9833a8442df1f1afa2fb1ee7f01
aqiang <1839992361@qq.com>
2025-01-06 19:35:34 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,38 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import WindiCSS from 'vite-plugin-windicss'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base:process.env.NODE_ENV === "production" ? "./" : "/",
-  plugins: [
-    vue(),
-    vueDevTools(),
-    WindiCSS()
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
 
-  server: {
-    proxy: {
-      [`^${process.env.VITE_API_BASE_URL}`]: {
-        target: process.env.VITE_API_TARGET_URL, // 目标地址
-        changeOrigin: true,
-        rewrite: (path) =>
-          path.replace(new RegExp(`^${process.env.VITE_API_BASE_URL}`), ''),
+  return {
+    // base 路径不影响开发环境
+    base: mode === "production" ?  "/" : "/",
+
+    plugins: [
+      vue(),
+      vueDevTools(),
+      WindiCSS()
+    ],
+
+    server: {
+      proxy: {
+        // 仅代理以 VITE_API_BASE_URL 开头的请求
+        [`${env.VITE_API_BASE_URL}`]: {
+          target: env.VITE_API_TARGET_URL, // 目标地址
+          changeOrigin: true, // 允许跨域
+          secure: false, // 如果使用的是 HTTPS 并且自签证书，设为 false
+          rewrite: (path) => path.replace(new RegExp(`^${env.VITE_API_BASE_URL}`), ''),
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+    }
   }
 })
