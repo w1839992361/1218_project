acaf4a0e7e7a6a8742c3ad081e7ea783ea5a8a22
aqiang <1839992361@qq.com>
2025-01-12 22:47:35 +0800

    update

--- src/views/manage/getData/index.vue
+++ src/views/manage/getData/index.vue
@@ -6,28 +6,7 @@ import { getTagsByParentId, getTagsByLevel, getTagsById } from "@/api/other";
 const exporting = ref(false);
 const version = ref();
 const selectedIds = ref([]);
-const exportIds = ref([
-  {
-    id: 1,
-    label: "课程教学",
-  },
-  {
-    id: 2,
-    label: "",
-  },
-  {
-    id: 3,
-    label: "",
-  },
-  {
-    id: 4,
-    label: "",
-  },
-  {
-    id: 5,
-    label: "",
-  },
-]); // 示例导出ID
+const exportIds = ref([]); // 示例导出ID
 
 const createVersion = async () => {
   try {
@@ -48,8 +27,13 @@ const handleExport = async () => {
     message.error("请至少选择一个导出ID");
     return;
   }
+  // 将版本转为数字，自增1，然后转换为字符串
+  let newVersion = (parseFloat(version.value) + 1).toFixed(1);
 
+// 调用函数并传递新的版本号
+  await createAUpdate({ version: newVersion });
   exporting.value = true;
+  getVersion();
 
   try {
     // 仅导出一次SQL文件
@@ -99,6 +83,7 @@ async function getExportId() {
   let arr = []; // 用于存储所有的id和name
   const { data } = await getTagsByParentId(1);
   const data2 = await getTagsByParentId(3);
+  const data3 = await getTagsByLevel(0);
 
   // 收集data中的id和name
   if (Array.isArray(data)) {
@@ -109,7 +94,17 @@ async function getExportId() {
   if (Array.isArray(data2.data)) {
     arr.push(...data2.data.map((item) => ({ id: item.id, name: item.name })));
   }
-  exportIds.value = arr;
+
+  // 收集data3中的id和name
+  if (Array.isArray(data3.data)) {
+    arr.push(...data3.data.map((item) => ({ id: item.id, name: item.name })));
+  }
+
+  // 使用 Set 去重（基于 id 和 name 的字符串化比较）
+  const uniqueArr = Array.from(
+    new Set(arr.map((item) => JSON.stringify(item)))
+  ).map((item) => JSON.parse(item));
+  exportIds.value = uniqueArr;
 }
 getExportId();
 getVersion();
@@ -125,20 +120,22 @@ getVersion();
         class="text-center min-w-[800px]"
       >
         <p>当前版本: {{ version }}</p>
-        <span class="text-[red]">导出过程中,请不要关闭页面！</span>
-        <div>
+        <span class="text-[red]">(导出过程中,请不要关闭页面！)</span>
+        <!-- <div>
           <a-button class="mt-5" @click="createVersion">
             创建新版本(先创建新版本,在选择导出)
           </a-button>
-        </div>
+        </div> -->
 
         <!-- 多选框列表 -->
-        <div v-for="item in exportIds" :key="item.id" class="m-2">
-          <label>
-            <input type="checkbox" :value="item.id" v-model="selectedIds" /> 导出:
-            {{ item.name }}
-          </label>
-        </div>
+        <a-row :gutter="16">
+          <a-col :span="6" v-for="item in exportIds" :key="item.id">
+            <label>
+              <input type="checkbox" :value="item.id" v-model="selectedIds" /> 导出:
+              {{ item.name }}
+            </label>
+          </a-col>
+        </a-row>
 
         <a-button class="mt-5" :loading="exporting" @click="handleExport">
           {{ exporting ? "导出中..." : "导出所选版本（zip和sql）" }}
